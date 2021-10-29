f127074c3e4a425215338187cc6009bc138da95f
trevclaridge <trevclaridge@gmail.com>
2021-10-29 16:01:39 -0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Trevor Claridge'),
     );
   }
 }
