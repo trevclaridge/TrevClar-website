0277e7f8658e4acd156c5481ca20c019f341fe21
Trevor Claridge <trevclaridge@gmail.com>
2022-05-01 01:38:56 -0700

    palette

---
+++ lib/view/theme/pallete.dart
@@ -0,0 +1,8 @@
+part of view;
+
+class Palette {
+  static const Color background = Color(0xFF0f1a40);
+  static const Color text = Color(0xFF7EA1C4);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color accent = Color(0xFF00d9c3);
+}
--- lib/view/view.dart
+++ lib/view/view.dart
@@ -5,4 +5,5 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 part 'theme/theme.dart';
+part 'theme/pallete.dart';
 part 'screens/home_page.dart';
